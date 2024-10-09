a9116544047855d1f5f5332dfc1eb5518a715a40
FrankyFP <78322925+FrankyFP@users.noreply.github.com>
2024-10-09 10:18:38 +0200

    Add files via upload

---
+++ Javascript/Konami.js
@@ -0,0 +1,49 @@
+// El codigo konami va aquí!
+let keys = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down",
+    65: "a",
+    66: "b",
+    13: "enter"
+};
+
+let konamiCode = ["up", "up", "down", "down", "left", "right", "left", "right", "b", "a", "enter"];
+let keyCount = 0;
+
+let timerID;
+
+document.addEventListener("keydown", checkCode, false);
+
+function checkCode(e) {
+    let keyPressed = keys[e.keyCode];
+
+    if (keyPressed === konamiCode[keyCount]) {
+        keyCount++;
+
+        // Reseteamos el timer
+        window.clearTimeout(timerID);
+
+        // Empezar un timer con 1 segundo de espera
+        timerID = window.setTimeout(resetKeyState, 1000);
+
+        // Mira si hay algun error en la combinacion
+        if (keyCount === konamiCode.length) {
+            cheatCodeActivated();
+            resetKeyState();
+        }
+    } else {
+        resetKeyState();
+    }
+}
+
+function cheatCodeActivated() {
+    location.href = "pagina.html";
+}
+
+function resetKeyState() {
+    console.log("Resetting state!");
+    window.clearTimeout(timerID);
+    keyCount = 0;
+}
